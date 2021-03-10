6542e4e3e357cd2f100cc6020954981874c31737
jambrosio7 <josue.ambrosio.81@my.csun.edu>
2021-03-10 14:48:59 -0800

    Revised Message

--- ascii2bin.c
+++ ascii2bin.c
@@ -7,38 +7,34 @@
 /* Validation Checks:           */
 /* Enhancements:                */
 /********************************/
-​
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <math.h>
-#include <string.h>
-#define byte unsigned char
-​
-int main (int argc, char * argv[], char ** envp) {
-​
-int offset = 0x30;
-int number;
-int retval;
-int digit;
-byte ascii_value;
-​
-retval = read(STDIN_FILENO, &ascii_value, 1);
-​
-while (retval == 1) {
-​
-    digit = ascii_value - offset;
-    number = (number << 1) + digit;
-    retval = read(STDIN_FILENO, &ascii_value, 1);
-    
+#include <unistd.h> 
+
+int main()
+{
+    int offset = 48;
+    int number = 0;
+    char ascii_value;
+
+    int retval = read(0, &ascii_value, 1);
+    while ((retval == 1) && (ascii_value != '\n')) {
+        int digit = ascii_value - offset;
+        if ((digit == 1) || (digit == 0)){
+            number = (number << 1) + digit;
+        retval = read(0, &ascii_value, 1);
     }
-    
-    printf("%d\n", number);
+
+    else {
+
+    return 1;
+
+    }
+
+    }
+
+    printf("%u\n", number);
+
     return 0;
-    
-    
-    if (retval == 0) { 
-    return 1;}
+
 }
-    
